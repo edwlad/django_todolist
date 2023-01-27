e31f20cb3a81b793db67b760a84048a7310a98b4
edwlad <80802093+edwlad@users.noreply.github.com>
2023-01-27 15:29:09 +0700

    v_0.1.0 add base and admin

--- main/admin.py
+++ main/admin.py
@@ -1,15 +1,48 @@
 from django.contrib import admin
+from django.db import models
 from .models import MyList
+from datetime import datetime
 
 
 @admin.register(MyList)
 class MyListAdmin(admin.ModelAdmin):
-    def chk_end(self, obj):
-        """Вычисляемое значение для только-читаемого поля."""
-        # return obj.date_end is not None
-        return obj.chk_end
+    # def chk_end(self, obj):
+    #     """Вычисляемое значение для только-читаемого поля."""
+    #     # return obj.date_end is not None
+    #     return obj.chk_end
 
     # list_display = tuple(v.attname for v in MyList._meta.fields)
-    list_display = ("id", "desc", "date_add", "date_end", "chk_end")
+    list_display = ("id", "desc", "date_add", "chk_end", "date_end")
+    fields = ("desc", "date_add", ("chk_end", "date_end"))
     search_fields = ("desc",)
-    readonly_fields = ("chk_end",)
+    # readonly_fields = ("date_end",)
+    # list_filter = ("date_add", "date_end")
+
+    actions = ("make_on_end", "make_off_end", "make_not_end")
+
+    @admin.action(description="Выполнить задачи")
+    def make_on_end(self, request, queryset: models.QuerySet):
+        queryset.update(chk_end=True, date_end=datetime.now())
+
+    @admin.action(description="Отменить выполнение задачи")
+    def make_off_end(self, request, queryset: models.QuerySet):
+        queryset.update(chk_end=False, date_end=None)
+
+    @admin.action(description="Изменить статус выполнения")
+    def make_not_end(self, request, queryset: models.QuerySet):
+        # for item in queryset:
+        #     item.chk_end = not item.chk_end
+        #     item.save()
+        queryset.update(
+            chk_end=models.Case(
+                models.When(chk_end=True, then=False),
+                default=True,
+            ),
+            date_end=models.Case(
+                models.When(chk_end=True, then=None),
+                default=datetime.now(),
+            ),
+        )
+
+
+# admin.site.register(MyList, MyListAdmin)
--- main/models.py
+++ main/models.py
@@ -1,27 +1,43 @@
 from django.db import models
 from datetime import datetime
-from .managers import MyListManager
+
+# from .managers import MyListManager
 
 
 class MyList(models.Model):
-    objects = MyListManager()
+    # objects = MyListManager()
 
     desc = models.CharField(
         max_length=120,
         help_text="Описание задачи",
+        verbose_name="Описание",
     )
     date_add = models.DateTimeField(
         default=datetime.now,
         help_text="Дата создания задачи",
+        verbose_name="Создано",
         db_index=True,
     )
     date_end = models.DateTimeField(
         default=None,
-        help_text="Дата выполнения задачи",
+        help_text="Дата завершения задачи",
+        verbose_name="Завершено",
         blank=True,
         null=True,
         db_index=True,
     )
+    chk_end = models.BooleanField(
+        default=False,
+        help_text="Завершить",
+        verbose_name="",
+    )
 
     def __str__(self) -> str:
-        return " | ".join(f"{v}: {getattr(self, v)}" for v in ("id", "desc"))
+        return f"{self.id}: {self.desc}"
+
+    def save(self, *args, **kwargs) -> None:
+        if self.chk_end and self.date_end is None:
+            self.date_end = datetime.now()
+        elif not self.chk_end and self.date_end is not None:
+            self.date_end = None
+        return super().save(*args, **kwargs)
